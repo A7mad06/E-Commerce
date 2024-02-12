dcbd76c20323adc4b878f17a785a22b641d4817d
ahmad <ahmadbustany06@gmail.com>
2024-02-12 17:21:17 +0200

    Cart Controller adding

---
+++ ecommerce.api/E-Commerce Api/Controllers/CartController.cs
@@ -0,0 +1,77 @@
+﻿using E_Commerce_Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
+
+namespace E_Commerce_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        string con = "Server=DESKTOP-ODD35L0\\SQLEXPRESS;Database=E-Commerce;Trusted_Connection=true";
+
+        [HttpPost]
+        [Route("{CustomerEmail}")]
+        public async Task<IActionResult> AddCart(Cart cart,[FromRoute] string CustomerEmail)
+        {
+            cart.Id = Guid.NewGuid();
+            cart.total = 0;
+            cart.Customer_Email = CustomerEmail;
+            SqlConnection sql = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand("Insert into Cart values('" + cart.Id + "','" + cart.total + "','" + CustomerEmail + "')", sql);
+            sql.Open();
+            cmd.ExecuteNonQuery();
+            sql.Close();
+            return Ok(cart);
+        }
+
+        [HttpPut]
+        [Route("Update-Cart/{Cart_id:Guid}")]
+        public async Task<IActionResult> UpdateCart([FromRoute] Guid Cart_id)
+        {
+            SqlConnection sql = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand("select Item_Subtotal from Order_Item where Cart_Id='"+Cart_id+"'", sql);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dt);
+            int total=0;
+            foreach (DataRow row in dt.Rows)
+            {
+                total += int.Parse(row[0].ToString());
+            }
+            SqlCommand cmdd = new SqlCommand("Update Cart set Cart_Total='" + total + "' where Cart_Id='"+Cart_id+"'", sql);
+            sql.Open();
+            cmdd.ExecuteNonQuery();
+            sql.Close();
+            SqlCommand cm = new SqlCommand("Select * from Cart where Cart_Id='"+Cart_id+"'", sql);
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter ad = new SqlDataAdapter(cm);
+            ad.Fill(dataTable);
+            List<Cart> lst = new List<Cart>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Cart cart = new Cart();
+                cart.Id = Guid.Parse(row[0].ToString());
+                cart.total = int.Parse(row[1].ToString());
+                cart.Customer_Email = row[2].ToString();
+                lst.Add(cart);
+            }
+            return Ok(lst);
+        }
+
+        [HttpDelete]
+        [Route("Delete-Cart/{id:Guid}")]
+        public async Task<IActionResult> DeletCart([FromRoute] Guid Id)
+        {
+            SqlConnection sql = new SqlConnection(con);
+            SqlCommand com = new SqlCommand("Delete from Cart where Cart_Id='"+Id+"'", sql);
+            sql.Open();
+            com.ExecuteNonQuery();
+            sql.Close();
+            return Ok();
+        }
+    }
+}
--- ecommerce.api/E-Commerce Api/Controllers/ItemController.cs
+++ ecommerce.api/E-Commerce Api/Controllers/ItemController.cs
@@ -18,7 +18,7 @@ namespace E_Commerce_Api.Controllers
         {
             SqlConnection sql = new SqlConnection(con);
             List<Item> items = new List<Item>();
-            SqlCommand cmd = new SqlCommand("select * from Order_Item where Card_Id='"+cart_id+"'",sql);
+            SqlCommand cmd = new SqlCommand("select * from Order_Item where Cart_Id='"+cart_id+"'",sql);
             DataTable dt = new DataTable();
             SqlDataAdapter ad   = new SqlDataAdapter(cmd);
             ad.Fill(dt);
@@ -32,6 +32,7 @@ namespace E_Commerce_Api.Controllers
                 item.Description = dr[5].ToString();
                 item.Cart_Id = Guid.Parse(dr[6].ToString());
                 item.subtotal= int.Parse(dr[7].ToString());
+                item.Product_Name = dr[8].ToString();
                 items.Add(item);
             }
             return Ok(items);
@@ -57,8 +58,8 @@ namespace E_Commerce_Api.Controllers
             }
             item.Id = Guid.NewGuid();
             item.Quantity= 1;
-            item.subtotal = (item.Quantity * item.Price);
-            SqlCommand cmd = new SqlCommand("insert into Order_Item(Order_Item_Id,Product_Id,Product_Quatity,Product_Price,Producr_Description,Cart_Id,item_Subtotal,Product_Name) values('"+item.Id+"','"+product.Id+"','"+item.Quantity+"','"+product.Price+"','"+product.Description+"','"+cart_id+"','"+item.subtotal+"','"+product.Name+"')", sql);
+            item.subtotal = (item.Quantity * product.Price);
+            SqlCommand cmd = new SqlCommand("insert into Order_Item(Order_Item_Id,Product_Id,Product_Quantity,Product_Price,Product_Description,Cart_Id,Item_Subtotal,Product_Name) values('"+item.Id+"','"+product.Id+"','"+item.Quantity+"','"+product.Price+"','"+product.Description+"','"+cart_id+"','"+item.subtotal+"','"+product.Name+"')", sql);
             sql.Open();
             cmd.ExecuteNonQuery();
             sql.Close();
@@ -77,33 +78,41 @@ namespace E_Commerce_Api.Controllers
                 item1.Description = dr[5].ToString();
                 item1.Cart_Id = Guid.Parse(dr[6].ToString());
                 item1.subtotal = int.Parse(dr[7].ToString());
+                item1.Product_Name = dr[8].ToString();
                 items.Add(item1);
             }
             return Ok(items);
         }
         [HttpPut]
-        [Route("Add-Item/{Item_id:Guid}/{Product_id:Guid}")]
+        [Route("Add-Item/{Item_id:Guid}")]
 
-        public async Task<IActionResult> EditItem(Item item, [FromRoute] Guid item_id,Guid product_id)
+        public async Task<IActionResult> EditItem(Item item, [FromRoute] Guid Item_id)
         {
             SqlConnection sql = new SqlConnection(con);
             Product product = new Product();
-            product.Id = product_id;
-            SqlCommand cmdd = new SqlCommand("select * from Product where Product_Id='" + product_id + "'", sql);
+            SqlCommand cmdd = new SqlCommand("select Product_Id from Order_Item where Order_Item_Id='"+Item_id+"'", sql);
             DataTable dtt = new DataTable();
             SqlDataAdapter da = new SqlDataAdapter(cmdd);
             da.Fill(dtt);
             foreach (DataRow dr in dtt.Rows)
             {
-                product.Price = int.Parse(dr[5].ToString());
+                product.Id = Guid.Parse(dr[0].ToString());
             }
-            item.subtotal = (item.Quantity * item.Price);
-            SqlCommand cmd = new SqlCommand("Update Order_Item set Product_Quantity='"+item.Quantity+"',Subtotal='"+item.subtotal+"' where Order_Item_Id='"+item_id+"'", sql);
+            SqlCommand cmddd = new SqlCommand("select Product_Price from Product where Product_Id='"+product.Id+"'", sql);
+            DataTable dttt = new DataTable();
+            SqlDataAdapter daa = new SqlDataAdapter(cmddd);
+            daa.Fill(dttt);
+            foreach (DataRow dr in dttt.Rows)
+            {
+                product.Price = int.Parse(dr[0].ToString());
+            }
+            item.subtotal = (item.Quantity * product.Price);
+            SqlCommand cmd = new SqlCommand("Update Order_Item set Product_Quantity='"+item.Quantity+"',Item_Subtotal='"+item.subtotal+"' where Order_Item_Id='"+Item_id+"'", sql);
             sql.Open();
             cmd.ExecuteNonQuery();
             sql.Close();
             List<Item> items = new List<Item>();
-            SqlCommand command = new SqlCommand("select * from Order_Item where Order_Item_Id='" + item_id + "'", sql);
+            SqlCommand command = new SqlCommand("select * from Order_Item where Order_Item_Id='"+Item_id+"'", sql);
             DataTable dt = new DataTable();
             SqlDataAdapter ad = new SqlDataAdapter(command);
             ad.Fill(dt);
@@ -117,6 +126,7 @@ namespace E_Commerce_Api.Controllers
                 item1.Description = dr[5].ToString();
                 item1.Cart_Id = Guid.Parse(dr[6].ToString());
                 item1.subtotal = int.Parse(dr[7].ToString());
+                item1.Product_Name = dr[8].ToString();
                 items.Add(item1);
             }
             return Ok(items);
@@ -125,10 +135,22 @@ namespace E_Commerce_Api.Controllers
         [HttpDelete]
         [Route("Delete-Item/{Item_id:Guid}")]
 
-        public async Task<IActionResult> DeleteItem([FromRoute] Guid item_id)
+        public async Task<IActionResult> DeleteItem([FromRoute] Guid Item_id)
         {
             SqlConnection sql = new SqlConnection(con);
-            SqlCommand cmd = new SqlCommand("Delete from Order_Item where Order_Item_Id='" + item_id + "'", sql);
+            SqlCommand cmd = new SqlCommand("Delete from Order_Item where Order_Item_Id='"+Item_id+"'", sql);
+            sql.Open();
+            cmd.ExecuteNonQuery();
+            sql.Close();
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("Delete-All/{Cart_id:Guid}")]
+
+        public async Task<IActionResult> DeleteAllItems([FromRoute] Guid Cart_id)
+        {
+            SqlConnection sql = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand("Delete from Order_Item where Cart_Id='"+Cart_id+"'", sql);
             sql.Open();
             cmd.ExecuteNonQuery();
             sql.Close();
